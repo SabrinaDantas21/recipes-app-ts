401055b5f3b9fe1ff594f762db69329ac6a7cf97
ALarissaLima <larissalima622@gmail.com>
2024-06-12 20:18:26 +0100

    Requisito 25

--- src/pages/Meals.tsx
+++ src/pages/Meals.tsx
@@ -1,17 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { setPage } from '../redux/actions';
 import Header from '../components/Header';
+import { getDrinksByFilter } from '../services/api';
 
 function Meals() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [drinksRecommendation, setDrinksRecommendation] = useState([]);
+
+  const getDrinksRecommendation = async () => {
+    const currentInfo = {
+      searchBarInfo: {
+        radioBtnValue: '',
+        searchBarValue: '',
+      },
+      navigate,
+    };
+    const data = await getDrinksByFilter(currentInfo);
+    setDrinksRecommendation(data);
+  };
 
   useEffect(() => {
     dispatch(setPage({
       title: 'Meals',
       showSearchIcon: true,
     }));
-  });
+    getDrinksRecommendation();
+  }, []);
 
   return (
     <Header page="meals" />
