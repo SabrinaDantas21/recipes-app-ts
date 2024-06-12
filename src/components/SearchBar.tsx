f08a8059690b176de8cb2080142ef8b71bd91133
gustavoSdeSa <gustavo_s_sa@hotmail.com>
2024-06-12 10:31:21 -0300

    Cria o input de busca, os radios buttons e o botão do SerchBar.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import Input from './Input';
 
 function SearchBar() {
   return (
-    <Input data-testid="search-input" />
+    <div>
+      <Input
+        type="text"
+        data-testid="search-input"
+      />
+      <div>
+        <Input
+          data-testid="ingredient-search-radio"
+          type="radio"
+          name="filter-radio"
+          value="ingredient-radio"
+        />
+        <label htmlFor="ingredient-radio">Ingredient</label>
+      </div>
+      <div>
+        <Input
+          data-testid="name-search-radio"
+          type="radio"
+          name="filter-radio"
+          value="name-radio"
+        />
+        <label htmlFor="name-radio">Name</label>
+      </div>
+      <div>
+        <Input
+          data-testid="first-letter-search-radio"
+          type="radio"
+          name="filter-radio"
+          value="first-letter-radio"
+        />
+        <label htmlFor="first-letter-radio">First letter</label>
+      </div>
+      <Button data-testid="exec-search-btn">Search</Button>
+    </div>
   );
 }
 
