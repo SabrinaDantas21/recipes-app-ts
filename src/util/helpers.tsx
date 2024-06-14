111953f237631e3cf3f6deb63785fe8d0f90be4c
Márcio Mingante <marciomvp17@gmail.com>
2024-06-13 21:16:26 -0300

    arruma os reqs 11,12

--- src/util/helpers.tsx
+++ src/util/helpers.tsx
@@ -1,4 +1,16 @@
-export const verifyLength = (consumable: 'drink' | 'meal', data:any, navigate:any) => {
+import { NavigateFunction } from 'react-router-dom';
+import { DrinkObjectType, MealObjectType } from './types';
+
+type ApiDataType = {
+  meals?: MealObjectType[]
+  drinks?: DrinkObjectType[]
+};
+
+export const verifyLength = (
+  consumable: 'drink' | 'meal',
+  data: ApiDataType,
+  navigate: NavigateFunction,
+) => {
   if (consumable === 'drink' && data.drinks === null) {
     return window.alert('Sorry, we haven\'t found any recipes for these filters');
   }
@@ -7,11 +19,11 @@ export const verifyLength = (consumable: 'drink' | 'meal', data:any, navigate:an
     return window.alert('Sorry, we haven\'t found any recipes for these filters');
   }
 
-  if (consumable === 'drink' && data.drinks.length === 1) {
+  if (consumable === 'drink' && data.drinks?.length === 1) {
     return navigate(`/drinks/${data.drinks[0].idDrink}`);
   }
 
-  if (consumable === 'meal' && data.meals.length === 1) {
+  if (consumable === 'meal' && data.meals?.length === 1) {
     return navigate(`/meals/${data.meals[0].idMeal}`);
   }
   return data;
