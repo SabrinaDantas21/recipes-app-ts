d4b1054f4dec8fe9eb4a0488cb084ad0905ab455
gustavoSdeSa <gustavo_s_sa@hotmail.com>
2024-06-14 14:47:41 -0300

    Salva progresso até o momento.

---
+++ src/pages/InProgressRecipes.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RecipeDetails from '../components/RecipeDetails';
+
+function InProgressRecipes() {
+  return (
+    <RecipeDetails />
+  );
+}
+
+export default InProgressRecipes;
--- src/util/types.ts
+++ src/util/types.ts
@@ -75,6 +75,18 @@ export type ButtonType = {
   className?: string
 };
 
+export type DoneRecipeType = {
+  id: string;
+  type: string;
+  nationality: string;
+  category: string;
+  alcoholicOrNot: string;
+  name: string;
+  image: string;
+  doneDate: string;
+  tags: string;
+};
+
 // -----------Tipagem do Thunk -----------
 
 export type DispatchType = ThunkDispatch<GlobalStoreType, unknown, AnyAction>;
