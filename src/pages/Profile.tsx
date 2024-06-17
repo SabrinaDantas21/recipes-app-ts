75339b5c6e2cc8d1c40568fdd525cf032987a0eb
SabrinaDantas21 <brinahdantas56@gmail.com>
2024-06-17 20:02:11 -0300

    do requisito 56 ao 61, só falta os testes e apagar o localstorage no botao logout

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setPage } from '../redux/actions';
 import Header from '../components/Header';
@@ -6,17 +7,41 @@ import Footer from '../components/Footer';
 
 function Profile() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleChangeLogout = () => {
+    localStorage.setItem('user', 'count');
+    localStorage.removeItem('user');
+    localStorage.removeItem('count');
+  };
 
   useEffect(() => {
     dispatch(setPage({
       title: 'Profile',
       showSearchIcon: false,
     }));
-  });
+  }, [dispatch]);
 
   return (
     <>
       <Header />
+      <h3 data-testid="profile-email">
+        {localStorage.getItem('user')}
+      </h3>
+      <button type="button" data-testid="profile-done-btn">
+        <Link to="/done-recipes">Done Recipes</Link>
+      </button>
+      <button type="button" data-testid="profile-favorite-btn">
+        {' '}
+        <Link to="/favorite-recipes">Favorite Recipes</Link>
+      </button>
+      <button
+        type="reset"
+        data-testid="profile-logout-btn"
+        onChange={ handleChangeLogout }
+      >
+        <Link to="/">Logout</Link>
+      </button>
       <Footer />
     </>
   );
