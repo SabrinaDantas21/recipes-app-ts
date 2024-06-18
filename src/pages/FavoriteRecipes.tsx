9f4c0a6aa1fdeaa99b746994ac0a1fb19ccec7b5
Ricardo Alexandre <rr147519@gmail.com>
2024-06-18 18:01:27 -0300

    Criando a pagina FavoriteRecipes

--- src/pages/FavoriteRecipes.tsx
+++ src/pages/FavoriteRecipes.tsx
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setPage } from '../redux/actions';
 import Header from '../components/Header';
+import Button from '../components/Button';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function FavoriteRecipes() {
   const dispatch = useDispatch();
@@ -14,7 +17,28 @@ function FavoriteRecipes() {
   });
 
   return (
-    <Header />
+    <>
+      <Header />
+      <div>
+        <Button
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </Button>
+        <Button
+          data-testid="filter-by-meal-btn"
+        >
+          <img src={ mealIcon } alt="" />
+          Meals
+        </Button>
+        <Button
+          data-testid="filter-by-drink-btn"
+        >
+          <img src={ drinkIcon } alt="" />
+          Drinks
+        </Button>
+      </div>
+    </>
   );
 }
 
