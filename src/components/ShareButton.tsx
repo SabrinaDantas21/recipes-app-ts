6931bb3d37d498a33843a8ff0fc4fe76e4817b92
ALarissaLima <larissalima622@gmail.com>
2024-06-19 07:30:15 +0100

    test

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -5,14 +5,10 @@ import shareBtn from '../images/shareIcon.svg';
 function ShareButton({ dataTestidBtn = '', url = '' }) {
   const [msgIsVisible, setMsgIsVisible] = useState(false);
 
-  const readClipBoard = () => {
-    navigator.clipboard.readText().then((clipText) => console.log(clipText));
-  };
   const handleShareBtn = async () => {
     if (msgIsVisible === false) {
       await navigator.clipboard.writeText(url);
       setMsgIsVisible(true);
-      readClipBoard();
     } else {
       setMsgIsVisible(false);
     }
